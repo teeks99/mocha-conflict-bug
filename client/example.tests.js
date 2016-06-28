fd522e347527eafdb35cf440555e791aeafd0e63
Thomas Kent <teeks99@yahoo.com>
2016-06-28 17:16:17 -0500

    Working copy

---
+++ client/example.tests.js
@@ -0,0 +1,13 @@
+import { assert } from 'meteor/practicalmeteor:chai';
+
+describe('Client Tasks', () => {
+  describe('methods', () => {
+    it('client example test method', () => {
+      var value = 42;
+      var expected = 42;
+      
+      assert.equal(expected, value);
+    });
+  });
+});
+    
---
+++ server/example.tests.js
@@ -0,0 +1,13 @@
+import { assert } from 'meteor/practicalmeteor:chai';
+
+describe('Server Tasks', () => {
+  describe('methods', () => {
+    it('example test method', () => {
+      var value = 42;
+      var expected = 42;
+      
+      assert.equal(expected, value);
+    });
+  });
+});
+    
